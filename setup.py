0bc5dfe8b38173070ab48b76ce5c3bd2bcea8c8a
ARU300 <ARU300@habsboys.org.uk>
2020-07-28 22:07:00 +0100

    PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
@@ -7,13 +7,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='Summer-Project',
-    version='v1.0',
+    version='v1.2',
     packages=['Stock_Analysis'],
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='ARU300',
     author_email='ath.projalph@gmail.com',
     description='Summer Project',
-    url = 'https://github.com/ARU300/SummerProject/archive/v1.0.zip',
-    keywords = ['Machine Learning', 'Python3'],
-    classifiers = [],
+    url='https://github.com/ARU300/SummerProject/archive/v1.0.zip',
+    keywords=['Machine Learning', 'Python3'],
+    classifiers=[],
 )
